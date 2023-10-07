ed75984c3e1f8129f187b7bdbba69f1c364e337d
Mufeez Amjad <mufeez.amjad@outlook.com>
2023-10-07 00:10:42 -0400

    Brew formula update for lin version v0.0.5

--- lin.rb
+++ lin.rb
@@ -5,20 +5,20 @@
 class Lin < Formula
   desc ""
   homepage "https://github.com/mufeez-amjad/lin"
-  version "0.0.4"
+  version "0.0.5"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mufeez-amjad/lin/releases/download/v0.0.4/lin_0.0.4_darwin_arm64.tar.gz"
-      sha256 "c18653697f81cdc1e39da41f30cd151f25dc9b334ee02de1317795769b350b33"
+      url "https://github.com/mufeez-amjad/lin/releases/download/v0.0.5/lin_0.0.5_darwin_arm64.tar.gz"
+      sha256 "f8bb30f9debc147a032b61e664d8aa863a504573e018a81f6d2f7865bf79703e"
 
       def install
         bin.install "lin"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/mufeez-amjad/lin/releases/download/v0.0.4/lin_0.0.4_darwin_amd64.tar.gz"
-      sha256 "6378c7d5a3442da7314d144f804c0eec2f8c725f9b8484308b3e040df4a5ad88"
+      url "https://github.com/mufeez-amjad/lin/releases/download/v0.0.5/lin_0.0.5_darwin_amd64.tar.gz"
+      sha256 "959fb05e454c5bfff3acf1c3c0d824842937c6b616faf941340914548549d07c"
 
       def install
         bin.install "lin"
@@ -28,16 +28,16 @@ class Lin < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mufeez-amjad/lin/releases/download/v0.0.4/lin_0.0.4_linux_amd64.tar.gz"
-      sha256 "3633b3c1d0925c207142382830c2a700a1d6004d0e332654d40ec770868a600a"
+      url "https://github.com/mufeez-amjad/lin/releases/download/v0.0.5/lin_0.0.5_linux_amd64.tar.gz"
+      sha256 "eccbfade20d3ab8d7d437f5746a17bcab078e10af3cb4b4b58d73e143518454b"
 
       def install
         bin.install "lin"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mufeez-amjad/lin/releases/download/v0.0.4/lin_0.0.4_linux_arm64.tar.gz"
-      sha256 "c7c88caa630f64597c416225d47f83f5eeca9f8554dbfed8cf6c26b231313f66"
+      url "https://github.com/mufeez-amjad/lin/releases/download/v0.0.5/lin_0.0.5_linux_arm64.tar.gz"
+      sha256 "96c7fd51301604c333c81dc9a0ee1e6bed76a3dc28d6acf83dfc7f084f9a4609"
 
       def install
         bin.install "lin"
